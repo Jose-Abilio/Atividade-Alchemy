d9e5d9e4feef7028f256f427540d07615a4f4041
jose <Jabilio5353@gmail.com>
2025-08-07 08:27:43 -0300

    criação do banco

--- app.py
+++ app.py
@@ -4,44 +4,51 @@ from flask import flash
 
 from flask_login import LoginManager, UserMixin, logout_user
 from flask_login import login_required, login_user, current_user
-
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
-
 import sqlite3
 
+from flask_sqlalchemy import SQLAlchemy
+
+
 login_manager = LoginManager()
-
+#inicializando o flask e o alchemy
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+db = SQLAlchemy()
+db.init_app(app)
 login_manager.__init__(app)
 
 app.secret_key = 'chave_secreta'
 
+#criando as tabelas
+class usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(40),nullable = False, unique = True)
+    senha = db.Column(db.String(40),nullable = False)
 
-def obter_conexao():
-    conn = sqlite3.connect('banco.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+#def obter_conexao():
+    #conn = sqlite3.connect('banco.db')
+    #conn.row_factory = sqlite3.Row
+    #return conn
 
 class User(UserMixin):
     def __init__(self, nome, senha) -> None:
         self.nome = nome
         self.senha = senha
 
-    @classmethod
-    def get(cls, user_id):
+    #@classmethod
+    #def get(cls, user_id):
         # user_id nesse caso é um nome
-        conexao = obter_conexao()        
-        sql = "select * from users where nome = ?"
-        resultado = conexao.execute(sql, (user_id,)).fetchone()
-        user = User(nome=resultado['nome'], senha=resultado['senha'])
-        user.id = resultado['nome']
-        return user
+        #conexao = obter_conexao()        
+        #sql = "select * from users where nome = ?"
+        #resultado = conexao.execute(sql, (user_id,)).fetchone()
+        #user = User(nome=resultado['nome'], senha=resultado['senha'])
+        #user.id = resultado['nome']
+        #return user
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+
 
 @app.route('/')
 def index():
@@ -52,58 +59,18 @@ def login():
     if request.method == 'POST':
         nome = request.form['name']
         senha= request.form['password']
-
-        lista_usuarios = session['usuarios']
-
-        for id, dados in lista_usuarios.items():
-            hash = dados['senha']
-            if nome == dados['nome'] and check_password_hash(hash,senha):
-                user = User(nome=nome, senha=dados['senha'])
-                user.id = id
-                login_user(user)
-                return redirect(url_for('dash'))
-
-        flash('Dados incorretos', category='error')
-        return redirect(url_for('login'))
-
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
     if request.method == 'POST':
         nome = request.form['name']
         senha= request.form['password']
-    
-        conexao = obter_conexao()        
-        sql = "select * from users where nome = ?"
-        resultado = conexao.execute(sql, (nome,)).fetchone()
-        # none
-        if not resultado:
-            # realização do cadastro
-            sql = "INSERT INTO users(nome, senha) VALUES(?,?)"
-            conexao.execute(sql, (nome, senha))
-            conexao.commit()
-
-            # definir o usuário para logar
-            user = User(nome=nome, senha=senha)
-            user.id = nome
-
-            login_user(user)
-
-            # flash('Cadastro realizado com sucesso!', category='error')
-            return redirect(url_for('dash'))
-
-        conexao.close()
         
-        flash('Problema no cadastro', category='error')
-        return redirect(url_for('register'))
-
     return render_template('register.html')
 
 
 @app.route('/dashboard')
-@login_required
 def dash():
     return render_template('dash.html')
 
@@ -112,3 +79,7 @@ def dash():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
